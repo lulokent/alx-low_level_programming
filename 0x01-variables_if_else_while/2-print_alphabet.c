3d74b839531b1ae01945595803850f7298bcb629
lulokent <cwengalulonkem@gmail.com>
2023-10-06 14:49:47 -0700

    Alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
  * main-Entry point
  * return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 char ch;
-for ( ch = 'a' ;ch <= 'z' ;ch++)
+for(ch = 'a';ch <= 'z';ch++)
 {
-putchar (ch);
+putchar(ch);
 }
-putchar ('\n');
+putchar('\n');
 return (0);
 }
