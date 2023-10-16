42867724a2dc2f98f9646b94299d5de147e87d68
lulokent <cwengalulonkem@gmail.com>
2023-10-16 14:59:59 -0700

    Prints a string,followed by a newline to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: string to pring
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
