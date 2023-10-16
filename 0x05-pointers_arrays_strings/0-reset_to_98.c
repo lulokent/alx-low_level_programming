9c32904b60e873da3b5e4efe63974d1c4a46653a
lulokent <cwengalulonkem@gmail.com>
2023-10-16 14:02:15 -0700

    function that takes pointer nand updates the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that resets n to 98
+ * @n: pointer to the value to be updated
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learn about pointers,arrays and strings and how to use and manipulate them.
