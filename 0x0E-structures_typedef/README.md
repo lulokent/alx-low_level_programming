1d3ffc863d66ee35951938c9ab1bcc1e46dd8a6e
lulokent <cwengalulonkem@gmail.com>
2023-11-06 17:14:59 -0800

    Defining a new type struct dog withh dog's basic info

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+learning and undertsanding structures on when,why ans how to use them.
+also understanding how to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Desccription: longer despriction
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
