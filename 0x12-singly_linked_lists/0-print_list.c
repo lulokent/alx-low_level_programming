5695a2e6d687c8aefea711b53c3633ca90e69699
lulokent <cwengalulonkem@gmail.com>
2023-11-19 11:10:45 -0800

    prints all the elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
-
-	while (h)
+	if (h)
 	{
-	if (!h->str)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
+		if (h->str)
+			printf("[%d]%s\n", h->len, h->str);
+		else
+			printf("[%d]%s\n", 0, "(nil)");
+		if (h->next)
+		{
+			return (1 + print_list(h->next));
+		}
+		return (1);
 	}
-
-	return (s);
+	return (0);
 }
