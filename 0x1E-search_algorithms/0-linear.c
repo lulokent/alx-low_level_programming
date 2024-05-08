0d2d4a8660a83417cb7965c83a5c15c4ef63566c
lulokent <cwengalulonkem@gmail.com>
2024-05-08 09:24:05 -0700

    file 0 fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,9 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (array == NULL)
+	{
 		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
@@ -24,5 +26,5 @@ int linear_search(int *array, size_t size, int value)
 			return (i);
 		}
 	}
-	return (i);
+	return (-1);
 }
